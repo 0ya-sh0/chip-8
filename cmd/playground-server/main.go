430c5db302c2fa8acd41a19ed83147145927e3c8
0ya-sh0 <ymthatte.gmail.com>
2026-08-18 20:13:40 +0530

    Add WebSocket server and client implementations using Gorilla WebSocket

---
+++ cmd/playground-server/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	http.HandleFunc("/echo", echo)
+	err := http.ListenAndServe("localhost:9000", nil)
+	log.Fatal(err)
+}
+
+var upgrader = websocket.Upgrader{}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
