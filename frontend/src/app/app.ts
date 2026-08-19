f9e305eee46c1089162e50f02a317c18168a7121
vineray <neisha.random.online@protonmail.com>
2026-08-19 21:40:00 +0530

    send keyboard events to websocket server
    listen to keyup and keydown events and refer to the map to send the relevant Chip-8 key code to the server.
    server acts on the message sent.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, DestroyRef, HostListener, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WebSocketService } from './services/websocket.service';
 import { firstValueFrom, Subscription } from 'rxjs';
@@ -18,8 +18,55 @@ export class App implements OnInit {
   private soundService_ = inject (SoundService);
   protected readonly title = signal('frontend');
   public gameState: Array<Array<boolean>> = [];
+  
+  storedKeyBindings: { "1": string; "2": string; "3": string; C: string; "4": string; "5": string; "6": string; D: string; "7": string; "8": string; "9": string; E: string; A: string; "0": string; B: string; F: string; } = {
+    1: '',
+    2: '',
+    3: '',
+    C: '',
+    4: '',
+    5: '',
+    6: '',
+    D: '',
+    7: '',
+    8: '',
+    9: '',
+    E: '',
+    A: '',
+    0: '',
+    B: '',
+    F: ''
+  };
+  returnChip8Bindings: { [k: string]: string; } = {};
 
   async ngOnInit() {
+    this.loadKeyBindings();
+  }
+
+  loadKeyBindings() {
+    //This is how bindings will be stored as they will need to be against the chip 8 config. 
+    this.storedKeyBindings = {
+      "1": "Digit1",
+      "2": "Digit2",
+      "3": "Digit3",
+      "C": "Digit4",
+      "4": "KeyQ",
+      "5": "KeyW",
+      "6": "KeyE",
+      "D": "KeyR",
+      "7": "KeyA",
+      "8": "KeyS",
+      "9": "KeyD",
+      "E": "KeyF",
+      "A": "KeyZ",
+      "0": "KeyX",
+      "B": "KeyC",
+      "F": "KeyV",
+    };
+
+    this.returnChip8Bindings = Object.fromEntries(
+      Object.entries(this.storedKeyBindings).map(([key, value]) => [value, key])
+    );
   }
 
   private listenToWebSocket(){
@@ -66,5 +113,22 @@ export class App implements OnInit {
     }
     this.soundService_.unlockAudio();
     this.listenToWebSocket();
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyboardDown(event: KeyboardEvent): void {
+    this.sendKeypress(false, this.returnChip8Bindings[event.code]);
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  handleKeyboardUp(event: KeyboardEvent): void {
+    this.sendKeypress(true, this.returnChip8Bindings[event.code]);
+  }
+
+  sendKeypress(up: boolean, key: string) {
+    this.webSocketService_.send({
+      type: up ? "key.up" : "key.down",
+      data: key
+    })
   }
 }
