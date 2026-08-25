7f97a8c33987c58e8fc409621f87bfa4a59e1d24
0ya-sh0 <ymthatte.gmail.com>
2026-08-25 22:24:52 +0530

    refactor: remove playingSound state from PlaygroundProvider

--- cmd/playground-server/main.go
+++ cmd/playground-server/main.go
@@ -140,13 +140,12 @@ const (
 )
 
 type PlaygroundProvider struct {
-	mu           sync.Mutex
-	c            *websocket.Conn
-	inbox        <-chan KeyEvent
-	keys         [16]bool
-	playingSound bool
-	fbuff        atomic.Pointer[vm.FrameBuffer]
-	outbox       chan map[string]any
+	mu     sync.Mutex
+	c      *websocket.Conn
+	inbox  <-chan KeyEvent
+	keys   [16]bool
+	fbuff  atomic.Pointer[vm.FrameBuffer]
+	outbox chan map[string]any
 }
 
 func (p *PlaygroundProvider) keyProcessor() {
@@ -238,10 +237,6 @@ func jsonReader(c *websocket.Conn, ch chan<- KeyEvent) {
 
 // PlaySound implements [vm.SoundProvider].
 func (p *PlaygroundProvider) PlaySound() {
-	if p.playingSound {
-		return
-	}
-	p.playingSound = true
 	message := map[string]any{}
 	message["type"] = "sound.play"
 	p.outbox <- message
@@ -249,10 +244,6 @@ func (p *PlaygroundProvider) PlaySound() {
 
 // StopSound implements [vm.SoundProvider].
 func (p *PlaygroundProvider) StopSound() {
-	if !p.playingSound {
-		return
-	}
-	p.playingSound = false
 	message := map[string]any{}
 	message["type"] = "sound.stop"
 	p.outbox <- message
